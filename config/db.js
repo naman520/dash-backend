22617cc867d59795f08b2c5b7ea79eeaddedb1b4
bookmyassets <bookmyassets101@gmail.com>
2025-06-03 23:29:47 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,12 +1,20 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+/* const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+});
+ */
+
+const pool = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: `http://localhost:3001`,
+  origin: `https://dashboard-pearl-five-74.vercel.app`,
   credentials: true
 }));
 app.use(bodyParser.json());
