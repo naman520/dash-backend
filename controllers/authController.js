555de79f41133eafb643148e95f25709ef303a6b
bookmyassets <bookmyassets101@gmail.com>
2025-06-15 22:45:06 +0530

    changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -121,7 +121,7 @@ login: async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 3600000, 
     });
 
